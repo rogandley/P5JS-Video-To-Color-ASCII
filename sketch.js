07683f2102d0733be8a1d1a949fadccb7e03bc7f
Robert Gandley <36218331+rogandley@users.noreply.github.com>
2022-03-29 15:27:06 -0400

    Add p5.js files from project
    Short project based on Coding Train's video to standard ASCII tutorial- instead, create a colored ascii image.

---
+++ sketch.js
@@ -0,0 +1,39 @@
+//$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`'. 
+
+const density = '         .:-i|=+%O#@';
+
+let video;
+let asciiDiv;
+
+function setup() {
+  noCanvas();
+  video = createCapture(VIDEO);
+  video.size(72, 48);
+  asciiDiv = createDiv();
+}
+
+function draw() {
+  video.loadPixels();
+  
+  let asciiImage = "";
+  
+  for (let j = 0; j < video.height; j++) {
+    for (let i = 0; i < video.width; i++) {
+      const pixelIndex = (i + j * video.width) * 4;
+      const r = video.pixels[pixelIndex + 0];
+      const g = video.pixels[pixelIndex + 1];
+      const b = video.pixels[pixelIndex + 2];
+      const avg = (r + g + b) / 3;
+      
+      const len = density.length;
+      const charIndex = floor(map(avg, 0, 255, len, 0));
+      
+      const c = density.charAt(charIndex);
+      if (c == " ") asciiImage += "&nbsp;";
+      else asciiImage += '<span style="color: rgb(' + r + ',' + g + ',' + b + ')">' + c + '</span>';
+    }
+    asciiImage += '<br/>';
+    //console.log(row);
+  }
+  asciiDiv.html(asciiImage);
+}
